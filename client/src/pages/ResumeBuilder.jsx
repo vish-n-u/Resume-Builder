f9e50f24ef451b1583e63f8bc22c292646a77d88
vish-n-u-k <vishnu@kuchnaya.com>
2025-10-29 10:10:29 +0530

    min changes

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -358,9 +358,9 @@ const saveResume = async () => {
                   )}
 
               </div>
-              {/* <button onClick={()=> {toast.promise(saveResume, {loading: 'Saving...'})}} className='bg-gradient-to-br from-green-100 to-green-200 ring-green-300 text-green-600 ring hover:ring-green-400 transition-all rounded-md px-6 py-2 mt-6 text-sm'>
+              <button onClick={()=> {toast.promise(saveResume, {loading: 'Saving...'})}} className='bg-gradient-to-br from-green-100 to-green-200 ring-green-300 text-green-600 ring hover:ring-green-400 transition-all rounded-md px-6 py-2 mt-6 text-sm'>
                 Save Changes
-              </button> */}
+              </button>
             </div>
           </div>
 
