305b2962df4596c6bf1612108b12cf16b6e12ad4
Vishnu <vishnu@kuchnaya.com>
2026-03-15 12:01:49 +0530

    min change

--- client/src/components/JobDetail.jsx
+++ client/src/components/JobDetail.jsx
@@ -1,12 +1,6 @@
 import React from 'react'
 import { MapPin, DollarSign, ExternalLink, Sparkles } from 'lucide-react'
-
-const AVATAR_COLORS = [
-  'bg-blue-500', 'bg-purple-500', 'bg-rose-500',
-  'bg-emerald-600', 'bg-orange-500', 'bg-cyan-600',
-]
-const avatarColor = (company = '') =>
-  AVATAR_COLORS[company.charCodeAt(0) % AVATAR_COLORS.length]
+import { avatarColor } from '../utils/jobAvatar'
 
 const JobDetail = ({ job, onApply, onApplyExternal }) => {
   if (!job) {
